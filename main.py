fdcf5213617d89205deed665e880aa5f1673383f
Clement Chong <119944151+sussynk@users.noreply.github.com>
2026-04-28 12:48:51 +0800

    Added file format selection and some cosmetic improvement

--- main.py
+++ main.py
@@ -1,9 +1,29 @@
 import qrcode
 
-context = input(str("Content: "))
+context = str(input("Content: "))
+
+print("Select file format:")
+print("[1] JPG")
+print("[2] PNG")
+print("[3] WEBP")
+
+file_format_selection = int(input("File format: "))
+
+if file_format_selection == 1:
+    file_format = "jpg"
+elif file_format_selection == 2:
+    file_format = "png"
+elif file_format_selection == 3:
+    file_format = "webp"
+else:
+    print("Invalid selection")
 
 img = qrcode.make(context)
 
+file_name = str(input("File name: "))
+
 type(img)
 
-img.save("code.png")
+img.save(f"{file_name}.{file_format}")
+
+print("QR Code Generated successfully! Thank you for using me.")
